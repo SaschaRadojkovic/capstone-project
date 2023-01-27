a0024b63a35121b8bfa32e61b8862303f5fdb14f
sascha radojkovic <scha13@gmx.de>
2023-01-27 12:21:53 +0100

    add line 55 setResult(null)

--- components/BarcodeScanner/index.js
+++ components/BarcodeScanner/index.js
@@ -49,7 +49,14 @@ export default function BarcodeScanner() {
           />
         </>
       ) : (
-        <button onClick={() => setScanning(true)}>Try again!</button>
+        <button
+          onClick={() => {
+            setScanning(true);
+            setResult(null);
+          }}
+        >
+          Try again!
+        </button>
       )}
 
       {/* Found Product */}
