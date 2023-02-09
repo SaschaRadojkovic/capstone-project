6e6366522ddd2ee9e3efb0aba5fbcd5493cb1550
sascha radojkovic <scha13@gmx.de>
2023-02-09 13:40:37 +0100

    add media query for saved products

--- pages/products/index.js
+++ pages/products/index.js
@@ -15,6 +15,10 @@ const StyledAllCards = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 0.5rem;
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+    @media (min-width: 1024px) {
+        grid-template-columns: repeat(4, 1fr);
 `;
 const StyledImage = styled(Image)`
   width: 135px;
